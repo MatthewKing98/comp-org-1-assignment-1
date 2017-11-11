afde38bae84d0037243cd267426053779d73b21e
MatthewKing98 <matthewsky98@gmail.com>
2017-11-11 16:52:59 -0500

    Update assignment1MatthewKing.s
    Basic formatting included

--- assignment1MatthewKing.s
+++ assignment1MatthewKing.s
@@ -3,10 +3,16 @@
 	userInput:
 		.space 8 #8 bytes, 1 byte per possible character; Space is maximum size
 	
+	newLine:
+		.asciiz "\n"
+	
+	space:
+		.asciiz " "
+	
 	.text #Assembly instructions component
 main: #Start of code
 
-	li $a1, 9 #Specify max size for read string
+	li $a1, 10 #Specify max size for read string
 	la $a0, userInput #Set destination for read string
 	li $v0, 8 #Read String code loaded
 	syscall #Read string from user
@@ -15,10 +21,22 @@ main: #Start of code
 	lb $t1, 0($t0)
 	add $a0, $t1, $zero
 	li $v0, 11 #Output character code loaded
+	syscall
+	
+	la $t0, space
+	lb $t1, 0($t0)
+	add $a0, $t1, $zero
+	li $v0, 11 #Output character code loaded
 	syscall	#Output string
+	
+	la $t0, userInput
 	lb $t1, 7($t0)
 	add $a0, $t1, $zero
 	li $v0, 11 #Output character code loaded
+	syscall	#Output string
+	
+	la $a0, newLine #Set source for output string
+	li $v0, 4 #Output String code loaded
 	syscall	#Output string
 	
 	la $a0, userInput #Set source for output string
