ca7b313ac9e7c48801dd9e735f550aa268ecba83
MatthewKing98 <matthewsky98@gmail.com>
2017-11-11 15:54:14 -0500

    Update assignment1MatthewKing.s
    Code further commented
    space better defined
    first and last bytes of user entered data accessed

--- assignment1MatthewKing.s
+++ assignment1MatthewKing.s
@@ -1,8 +1,30 @@
-# ASSIGNMENT 1
-	.data
-	userInput: .asciiz ""
+# PROGRAM: ASSIGNMENT 1
+	.data #Data declaration component
+	userInput:
+		.space 8 #8 bytes, 1 byte per possible character; Space is maximum size
 	
-	.text
-main:
-	li $v0, 10
+	.text #Assembly instructions component
+main: #Start of code
+
+	li $a1, 9 #Specify max size for read string
+	la $a0, userInput #Set destination for read string
+	li $v0, 8 #Read String code loaded
+	syscall #Read string from user
+	
+	la $t0, userInput
+	lb $t1, 0($t0)
+	add $a0, $t1, $zero
+	li $v0, 11 #Output character code loaded
+	syscall	#Output string
+	lb $t1, 7($t0)
+	add $a0, $t1, $zero
+	li $v0, 11 #Output character code loaded
+	syscall	#Output string
+	
+	la $a0, userInput #Set source for output string
+	li $v0, 4 #Output String code loaded
+	syscall	#Output string
+
+exit:
+	li $v0, 10 #Exit code loaded
 	syscall	#Exit program
