9981f27db8063adbdabc98d0d48ace21cc4f2189
MatthewKing98 <matthewsky98@gmail.com>
2017-11-14 14:44:26 -0500

    Update assignment1MatthewKing.s
    Assignment has been formatted and unused code removed from file

--- assignment1MatthewKing.s
+++ assignment1MatthewKing.s
@@ -1,48 +1,38 @@
-# PROGRAM: ASSIGNMENT 1
-# Variables:
-# ----------
-# MAIN
-# $s0 CONST End of string - NULL
-# $s1 CONST Maximum size of string - STRSIZE
-# $s2 CONST Input Base - INBASE
-# $s3 CONST Output Base - OUTBASE
-# $t0 Cumulative sum - cumulativeSum
-# $t1 Address of decimal in string form - deciString
-#
-#
-# 
-# CHECKDATA
-# $a0 Starting address of string - argument1
-# $t0 Current digit address - curAdd
-# $t1 Current digit - curDigit
-# $t2 Digit counter - digitCount
-# $t3 Current digit invalid flag - digiInvalFlag
-# $t4 Invalid number flag - invalFlag
-# $t5 Current ascii limit - curLim
-# $v0 $t3, Invalid number flag - returnVar1
-################################################################
+#######################################################
+#######################################################
+## AUTHOR: Matthew King                              ##
+## COURSE: Computer Organization I                   ##
+## PROGRAM: ASSIGNMENT 1                             ##
+## FUNCTION: Converts Hexadecimal code to Decimal    ##
+#######################################################
+#######################################################
+
 	.data #Data declaration component
-	userInput:
+	userInput: #location were valid entry is stored
 		.space 8 #8 bytes, 1 byte per possible character; Space is maximum size
-	
-	newLine:
-		.asciiz "\n"
-	
-	space:
-		.asciiz " "
 		
-	inputPrompt:
-		.asciiz "Please enter a hexadecimal number:\n"
+	inputPrompt: #prompt to request valid number
+		.asciiz "\n>> Please enter a hexadecimal number [0-9, A-F, or a-f ONLY]:\n"
 	
-	inputErrorText:
-		.asciiz "\n\nInvalid entry [0-9,A-F, or a-f ONLY]\n\n"
+	inputErrorText: #error message for invalid user input
+		.asciiz "\n\n>> Invalid entry [0-9, A-F, or a-f ONLY]\n\n"
 		
-	outputStatement:
-		.asciiz "Decimal value: "
+	outputStatement: #statement preceeding the program output
+		.asciiz "\n>> Decimal value: "
 	
-	outputDecimal:
+	outputDecimal: #string version of number (stored here because the 32 bit register represents 2s compliment vs unsigned)
 		.space 10 #Largest expected value is 4294967295 (FFFFFFFF); 10 digits long
 	
+#####################################################
+# MODULE: main                                      #
+# PURPOSE: Initial driver for code                  #
+# $s0 CONST End of string - NULL                    #
+# $s1 CONST Maximum size of string - STRSIZE        #
+# $s2 CONST Input Base - INBASE                     #
+# $s3 CONST Output Base - OUTBASE                   #
+# $t0 Cumulative sum - cumulativeSum                #
+# $t1 Address of decimal in string form - deciString#
+#####################################################
 	.text #Assembly instructions component
 main: #Start of code
 	#variables intialized
@@ -51,58 +41,59 @@ main: #Start of code
 	li $s2, 16 #CONST INBASE = 16
 	li $s3, 10 #CONST OUTBASE = 10
 input:
-	la $a0, inputPrompt #Set output source to newLine
+	la $a0, inputPrompt #Set output source to input prompt
 	li $v0, 4 #Output String code loaded
-	syscall	#Output string
-	li $a1, 10 #Specify max size for read string
+	syscall	#request for user input
+	li $a1, 9 #Specify max size for read string read characters = ($a1 - 1) = 8 max
 	la $a0, userInput #Set destination for read string
 	li $v0, 8 #Read String code loaded
 	syscall #Read string from user
 	
 validityCheck:
 	jal CheckData #Verifies if userInput is a valid HEX value
-	bne $v0, $zero, inputError
+	bne $v0, $zero, inputError #if input is invalid, go to inputError
 
 decimalConversion:
-	la $a0, userInput
-	jal CalcuateDecimal
-	add $t0, $v0, $zero
+	la $a0, userInput #pass the user's input as an argument
+	jal CalcuateDecimal #convert the code to a decimal value
+	add $t0, $v0, $zero #load the returned cumulative sum into $t0
 	
 stringConversion:
-	add $a0, $t0, $zero
-	jal ConvertDecimalToString
-	add $t1, $v0, $zero
+	add $a0, $t0, $zero #load the cumulative sum as an argument
+	jal ConvertDecimalToString #stringify the cumulative sum
+	add $t1, $v0, $zero #load the returned cumulative sum address into $t1
 	
 output:
-	la $a0, newLine #Set output source to newLine
+	la $a0, outputStatement #Set output source to output statement
 	li $v0, 4 #Output String code loaded
-	syscall	#Output string
-	la $a0, outputStatement #Set output source to outputStatement
-	li $v0, 4 #Output String code loaded
-	syscall	#Output string
+	syscall	#Print preceeding message before the decimal value
 	add $a0, $t1, $zero #Set output source to cumulativeSum
-	li $v0, 4 #Output Unsigned Integer code loaded
-	syscall	#Output signed integer
+	li $v0, 4 #Output String code loaded
+	syscall	#Output unsigned cumulative sum as a string
 	
 exit:
 	li $v0, 10 #Exit code loaded
 	syscall	#Exit program
 	
 inputError:
-	la $a0, inputErrorText #Set output source to newLine
+	la $a0, inputErrorText #Set output source to input error message
 	li $v0, 4 #Output String code loaded
-	syscall	#Output string
-	j input
+	syscall	#Inform the user if invalid code entry
+	j input #loop back to ask user for valid input
+	
 
-# CHECKDATA
-# $a0 Starting address of string - argument1
-# $t0 Current digit address - curAdd
-# $t1 Current digit - curDigit
-# $t2 Digit counter - digitCount
-# $t3 Current digit invalid flag - digiInvalFlag
-# $t4 Invalid number flag - invalFlag
-# $t5 Current ascii limit - curLim
-# $v0 $t3, Invalid number flag - returnVar1
+#####################################################
+# MODULE: CheckData                                 #
+# PURPOSE: Check if entered code is valid           #
+# $a0 Starting address of string - argument1        #
+# $t0 Current digit address - curAdd                #
+# $t1 Current digit - curDigit                      #
+# $t2 Digit counter - digitCount                    #
+# $t3 Current digit invalid flag - digiInvalFlag    #
+# $t4 Invalid number flag - invalFlag               #
+# $t5 Current ascii limit - curLim                  #
+# $v0 $t3, Invalid number flag - returnVar1         #
+#####################################################
 	
 CheckData:
 	add $t0, $a0, $zero #sets digit address to leftmost slot
@@ -113,35 +104,29 @@ CheckData:
 		lb $t1, 0($t0) #loads new digit 
 		beq $t1, $s0, CheckDataEnd #If the value is End-of-String, exit loop
 		ifNotNull: #if(curDigit != NULL)
-			la $a0, space #Set output source to newLine
-			li $v0, 4 #Output String code loaded
-			syscall	#Output string
-			add $a0, $t1, $zero
-			li $v0, 1 #Output Integer code loaded
-			syscall	#Output integer
-			numberTest:
-				li $t5, 47 #set curLim to "0" - 1
-				slt $t3, $t1, $t5 #return 1 if digit is less than "0"
-				bne $t3, $zero, AddToInvalFlag
-				li $t5, 57 #set curLim to "9"
-				slt $t3, $t5, $t1 #return 1 if "9" is less than digit
-				beq $t3, $zero, AddToInvalFlag #digit is between "0" and "9"
+			#is the digit a number
+			li $t5, 47 #set curLim to "0" - 1
+			slt $t3, $t1, $t5 #return 1 if digit is less than "0"
+			bne $t3, $zero, AddToInvalFlag #digit is invalid as it is less than "0"
+			li $t5, 57 #set curLim to "9"
+			slt $t3, $t5, $t1 #return 1 if "9" is less than digit
+			beq $t3, $zero, AddToInvalFlag #digit is between "0" and "9"
 			
-			upperCaseTest:
-				li $t5, 64 #set curLim to "A" - 1
-				slt $t3, $t1, $t5 #return 1 if digit is less than "A"
-				bne $t3, $zero, AddToInvalFlag
-				li $t5, 70 #set curLim to "F"
-				slt $t3, $t5, $t1 #return 1 if "F" is less than digit
-				beq $t3, $zero, AddToInvalFlag
-				
-			lowerCaseTest:
-				li $t5, 96 #set curLim to "a" - 1
-				slt $t3, $t1, $t5 #return 1 if digit is less than "A"
-				bne $t3, $zero, AddToInvalFlag
-				li $t5, 102 #set curLim to "f"
-				slt $t3, $t5, $t1 #return 1 if "F" is less than digit
-				beq $t3, $zero, AddToInvalFlag
+			#is the digit a valid upper-case letter
+			li $t5, 64 #set curLim to "A" - 1
+			slt $t3, $t1, $t5 #return 1 if digit is less than "A"
+			bne $t3, $zero, AddToInvalFlag #digit is invalid as it is less than "A"
+			li $t5, 70 #set curLim to "F"
+			slt $t3, $t5, $t1 #return 1 if "F" is less than digit
+			beq $t3, $zero, AddToInvalFlag #digit is between "A" and "F"
+			
+			#is the digit a valid lower-case letter
+			li $t5, 96 #set curLim to "a" - 1
+			slt $t3, $t1, $t5 #return 1 if digit is less than "a"
+			bne $t3, $zero, AddToInvalFlag #digit is invalid as it is less than "a"
+			li $t5, 102 #set curLim to "f"
+			slt $t3, $t5, $t1 #return 1 if "f" is less than digit
+			beq $t3, $zero, AddToInvalFlag #digit is between "a" and "f"
 	AddToInvalFlag:
 		or $t4, $t3, $t4 #updates overall flag based on current digit check
 		bne $t4, $zero, CheckDataEnd #ends loop early if invalid number is found
@@ -149,26 +134,25 @@ CheckData:
 		addi $t2, $t2, 1 #increment digit counter
 		bne $s1, $t2, checkDataLoop #while digitCount != STRSIZE
 	
-	la $a0, newLine #Set output source to newLine
-	li $v0, 4 #Output String code loaded
-	syscall	#Output string
-	
 	CheckDataEnd:	
-		add $v0, $t4, $zero #load status into return value, v0
+		add $v0, $t4, $zero #load status into return register, $v0
 		jr $ra #end of function
 
-# CALCULATEDECIMAL
-# $a0 Starting address of string - argument1
-# $t0 Current digit address - curAdd
-# $t1 Current digit - curDigit
-# $t2 Digit counter - digitCount
-# $t3 Exponent progress tracker - expCounter
-# $t4 Multiplier - multiplier
-# $t5 Tracks maximum exponent of current digit - expMax
-# $t6 Digit decimal value - digiVal
-# $t7 Cumulative sum - cumulativeSum
-# $t8 Temp const holder - tempConstHolder
-# $v0 $t7, cumulative sum - returnVar1
+#####################################################
+# MODULE: CalcuateDecimal                           #
+# PURPOSE: Converts Hex code to a decimal number    #
+# $a0 Starting address of string - argument1        #
+# $t0 Current digit address - curAdd                #
+# $t1 Current digit - curDigit                      #
+# $t2 Digit counter - digitCount                    #
+# $t3 Exponent progress tracker - expCounter        #
+# $t4 Multiplier - multiplier                       #
+# $t5 Maximum exponent of current digit - expMax    #
+# $t6 Digit decimal value - digiVal                 #
+# $t7 Cumulative sum - cumulativeSum                #
+# $t8 Temp const holder - tempConstHolder           #
+# $v0 $t7, cumulative sum - returnVar1              #
+#####################################################
 
 CalcuateDecimal:
 	add $t0, $a0, $zero #sets digit address to leftmost slot
@@ -192,30 +176,29 @@ CalcuateDecimal:
 		addDecimalDigitsLoop:
 			li $t3, 0 #expCounter = 0
 			li $t4, 1 #multiplier = 1
-			multiplierLoop:
-				beq $t3, $t5, multiplierLoopEnd #execute until the multiplier is multiplied by 16^expMax
+			multiplierLoop: #calculates what power of 16 the digit is to be multiplied by
+				beq $t3, $t5, translateCharToInt #execute until the multiplier is multiplied by 16^expMax
 				mult $t4, $s2 #multiply by 16 (effectively raise by a power)
 				mflo $t4 #load result of multiplier * 16
 				addi $t3, $t3, 1 #increment exponent counter
 				j multiplierLoop
-			multiplierLoopEnd:
-			translateCharToInt:
+			translateCharToInt: #identifies what the decimal value is of the given character
 				li $t8, 58 #set curLim to "9" + 1
 				slt $t8, $t8, $t1  #return 1 if "9" is less than digit
-				bne $t8, $zero, notDigit
+				bne $t8, $zero, notDigit #character is a letter in "A-F"/"a-f"
 				addi $t1, $t1, -48 #lower value so that "0" = 0
-				j applyMultiplier
+				j applyMultiplier 
 				notDigit:
 					li $t8, 71 #set curLim to "F" + 1
 					slt $t8, $t8, $t1 #return 1 if "9" is less than digit
-					bne $t8, $zero, notUpper
+					bne $t8, $zero, notUpper #character is a letter in "a-f"
 					addi $t1, $t1, -55 #lower value so that "A" = 10
 				j applyMultiplier
 				notUpper:
 					li $t8, 103 #set curLim to "f" + 1
 					slt $t8, $t8, $t1 #return 1 if "9" is less than digit
 					addi $t1, $t1, -87 #lower value so that "a" = 10
-			applyMultiplier:
+			applyMultiplier: #multiplier actual decimal value by multiplier
 				mult $t4, $t1 #multiplier * digit value
 				mflo $t6 #loads result of multiplier * digit value
 				add $t7, $t7, $t6 #adds digit decimal value to cumulativeSum
@@ -226,38 +209,32 @@ CalcuateDecimal:
 				lb $t1, 0($t0) #loads next digit
 				j addDecimalDigitsLoop
 		addDecimalDigitsLoopEnd:
-			add $v0, $t7, $zero
+			add $v0, $t7, $zero #load cumulative sum into return register, $v0
 			jr $ra #end of function
 		
-
-# CONVERTDECIMALTOSTRING
-# $a0 Decimal to convert
-# $a1 Size of decimal
-# $t0 $a0, Decimal to convert - deciVal
-# $t1 Destination string of converted decimal - deciString
-# $t2 deciVal mod 10 - modResult
-# $t3 
-# $t4 Multiplier - multiplier
-# $t5 Tracks maximum exponent of current digit - expMax
-# $t6 Digit decimal value - digiVal
-# $t7 Cumulative sum - cumulativeSum
-# $t8 Temp const holder - tempConstHolder
-# $v0 Destination string of converted decimal - returnVar1		
+###########################################################
+# MODULE: ConvertDecimalToString                          #
+# PURPOSE: Stringifies decimal so it is read as unsigned  #
+# $a0 Decimal to convert                                  #
+# $a1 Size of decimal                                     #
+# $t0 $a0, Decimal to convert - deciVal                   #
+# $t1 Destination string of converted decimal - deciString#
+# $t2 deciVal mod 10 - modResult                          #
+# $v0 $t1, deciString - returnVar1                        #
+###########################################################	
 		
 ConvertDecimalToString:
-	add $t0, $a0, $zero
-	la $t1, outputDecimal
-	addi $t1, $t1, 10 #shift attention to the end of the string
-	addi $t1, $t1, -1
-	add $t2, $s0, $zero
+	add $t0, $a0, $zero #loads content of decimal to convert
+	la $t1, outputDecimal #sets destnation string of decimal conversion
+	addi $t1, $t1, 9 #shift attention to the end of the string
 	addToString:
-		divu $t0, $s3
-		mfhi $t2
-		mflo $t0
-		addi $t2, $t2, 48 #raise value so that 0 = "0"
-		sb $t2, 0($t1)
-		addi $t1, $t1, -1
-		bne $t0, $zero, addToString
-	addi $t1, $t1, 1
-	add $v0, $t1, $zero
+		divu $t0, $s3 #obtain the rightmost decimal digit of the decimal
+		mfhi $t2 #load rightmost digit
+		mflo $t0 #load decimal without the rightmost digit
+		addi $t2, $t2, 48 #raise value so that 0 = "0", 1 = "1", etc.
+		sb $t2, 0($t1) #save character-digit in the string
+		addi $t1, $t1, -1 #shift attention to the space left of previous
+		bne $t0, $zero, addToString #repeat until there are no digits remaining
+	addi $t1, $t1, 1 #readjust pointer to start of the string
+	add $v0, $t1, $zero #load string pointer into return register, $v0
 	jr $ra #end of function
